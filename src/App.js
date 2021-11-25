822f4cac792f66df86a2a0c112e5d94251e7a88a
liskasi <77645171+liskasi@users.noreply.github.com>
2021-11-25 22:44:05 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import {useEffect} from "react";
+import { ReactComponent as ItalyMap } from './it.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  useEffect(() => {
+    const ItalyMap = document.getElementById('italy-map-svg');
+    ItalyMap.addEventListener('click', onMapClick);  
+  }, [])
+
+  function onMapClick(e) {
+    const elemId = e.target.id;
+    const elemTag = e.target.tagName;
+    if (elemTag === 'path') {
+      console.log(e.target.className);
+      if (e.target.className && e.target.className.baseVal.includes('City_isActive')) {
+        e.target.classList.remove('City_isActive');
+      }
+      else {
+        e.target.classList.add('City_isActive');
+      }
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ItalyMap />
     </div>
   );
 }
